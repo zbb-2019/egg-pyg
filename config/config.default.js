42fa07aea6d557b763d38fb3bd1d4f7b37bed4a5
zbb <1102344710@qq.com>
2019-09-10 17:39:53 +0800

    项目初始化+完成获取首页轮播图接口

---
+++ config/config.default.js
@@ -0,0 +1,66 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+const path = require('path');
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1568003929940_3804';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // 多出来的配置==========
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: [ 'http://localhost:8080' ],
+  };
+
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  };
+
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      host: '127.0.0.1',
+      port: '3307',
+      user: 'root',
+      password: 'Zbb@123456',
+      database: 'egg-pyg',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+
+  config.static = {
+    prefix: '/public/',
+    dir: [ path.join(appInfo.baseDir, 'public'), path.join(appInfo.baseDir, 'publicData') ],
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
